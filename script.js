3cdf4e832d1e0837586db7f58b32fa135e11950b
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-02 23:58:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function volume_sphere() {
+	let r = document.getElementById("radius").value;
+	let input2 = document.getElementById("volume")
+	let cal = (4*Math.pai()*r*r*)/3
+	input2.value = cal;
     //Write your code here
   
 } 
