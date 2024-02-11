997929779dfab109a2b93902d4dde8134a1c4783
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-11 16:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function volume_sphere(e) {
 	e.preventDefault();
 	let r = document.getElementById("radius").value;
-	let input2 = document.getElementById("volume").value = ""
+	let input2 = document.getElementById("volume");
 
 
 
 var raio = parseInt(r);
 
 var volumeEsfera = (4/3) * Math.PI * Math.pow(raio, 3);
-	input2.value = volumeEsfera;
+	document.getElementById("volume").value  = volumeEsfera;
   
 } 
 
