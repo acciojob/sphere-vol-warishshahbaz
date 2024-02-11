3d002d81f9d29dea861441bf3c0a3ad35ca8f8a7
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-11 16:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function volume_sphere(e) {
 	e.preventDefault();
-	
-	 const radiusInput = document.getElementById("radius").value;
+	let r = document.getElementById("radius").value;
+	let input2 = document.getElementById("volume").value = ""
+const PI = 3.14159;
 
-    // Convert the input to a floating-point number
-    const radius = parseFloat(radiusInput);
 
-    // Validate the input: Check if the input is a non-negative number
-    if (isNaN(radius) || radius < 0) {
-        // If input is not valid, display 'NaN' in the output field
-        document.getElementById("output").value = 'NaN';
-    } else {
-        // Calculate the volume of the sphere
-        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+var raio = parseInt(r);
 
-        // Display the calculated volume rounded to four decimal places
-        document.getElementById("output").value = volume.toFixed(4);
-    }
+var volumeEsfera = (4/3) * PI * Math.pow(raio, 3);
+	input2.value = volumeEsfera;
   
 } 
 
